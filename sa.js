48565ebee8880d6ad716bc52a45cdb347f528ee1
Akramboy <71210921+Akramboy@users.noreply.github.com>
2022-04-26 05:25:52 +0300

    Add files via upload

---
+++ sa.js
@@ -0,0 +1,36 @@
+
+
+module.exports.run = async (bot, message, args) => {
+const v = require("../../bot.js")
+
+   a = v.editmsg(message,"**Ni**")
+    v.sleep(1000);
+   b = v.editmsg(message,a,"**Nice car**")
+    v.sleep(1000);
+   c = v.editmsg(message,"**😎*")
+    v.sleep(1000);
+   d = v.editmsg(message,"**💀 **")
+    v.sleep(1000);
+   e = v.editmsg(message,"🕶️")
+    v.sleep(1000);
+   f = v.editmsg(message,"**💀**")
+    v.sleep(1000);
+   g = v.editmsg(message,"**🥶**")
+    v.sleep(1000);
+   h = v.editmsg(message,"**⌨️**")
+    v.sleep(1000);
+   ı = v.editmsg(message,"**🕶️**")
+    v.sleep(1000);
+   i = v.editmsg(message,"**⚠️**")
+    v.sleep(1000);
+    v.sleep(1000);
+   j = v.editmsg(message,"**👍🏿**")
+    v.sleep(1000);
+   k = v.editmsg(message, "**🚀**")
+    v.sleep(1000);
+
+    }
+
+module.exports.config = {
+    command: "sa"
+}
