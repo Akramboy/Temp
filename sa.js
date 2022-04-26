853dcd5b9ebc0db86d98f577d520a77f1a6a6855
Akramboy <71210921+Akramboy@users.noreply.github.com>
2022-04-26 05:36:36 +0300

    Update sa.js

--- sa.js
+++ sa.js
@@ -3,31 +3,31 @@
 module.exports.run = async (bot, message, args) => {
 const v = require("../../bot.js")
 
-   a = v.editmsg(message,"**Ni**")
-    v.sleep(1000);
+   a = v.editmsg(message,"**Ni~**")
+    v.sleep(5500);
    b = v.editmsg(message,a,"**Nice car**")
-    v.sleep(1000);
+    v.sleep(5500);
    c = v.editmsg(message,"**😎*")
-    v.sleep(1000);
+    v.sleep(5500);
    d = v.editmsg(message,"**💀 **")
-    v.sleep(1000);
+    v.sleep(5500);
    e = v.editmsg(message,"🕶️")
-    v.sleep(1000);
+    v.sleep(5500);
    f = v.editmsg(message,"**💀**")
-    v.sleep(1000);
+    v.sleep(5500);
    g = v.editmsg(message,"**🥶**")
-    v.sleep(1000);
+    v.sleep(5500);
    h = v.editmsg(message,"**⌨️**")
-    v.sleep(1000);
+    v.sleep(5500);
    ı = v.editmsg(message,"**🕶️**")
-    v.sleep(1000);
+    v.sleep(5500);
    i = v.editmsg(message,"**⚠️**")
-    v.sleep(1000);
-    v.sleep(1000);
+    v.sleep(5500);
+    v.sleep(5500);
    j = v.editmsg(message,"**👍🏿**")
-    v.sleep(1000);
+    v.sleep(5500);
    k = v.editmsg(message, "**🚀**")
-    v.sleep(1000);
+    v.sleep(5500);
 
     }
 
